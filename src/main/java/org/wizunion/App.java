b87d518c837d0e9e96b7444920b8c2cd019a1931
lishulin <lishulin@le.com>
2019-12-14 11:08:15 +0800

    first commit

---
+++ src/main/java/org/wizunion/App.java
@@ -0,0 +1,13 @@
+package org.wizunion.;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/org/wizunion/AppTest.java
@@ -0,0 +1,20 @@
+package org.wizunion.;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
